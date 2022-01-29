26e3b1c3881fbe08f2d9cefcfb92a42b7e5a034b
alperenkarate <alperensozen96@gmail.com>
2022-01-29 21:10:33 +0300

    camera and controls

--- src/script.js
+++ src/script.js
@@ -1,7 +1,33 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
 
+
+/**
+ * Sizes
+ * 
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**Cursor */
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = - (event.clientY / sizes.height - 0.5);
+})
+
+// window.addEventListener('touchmove', (event) => {
+//     cursor.x = event.touches[0].pageX / sizes.width - 0.5;
+//     cursor.y = - (event.touches[0].pageY / sizes.height - 0.5);
+// })
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -13,25 +39,24 @@ const scene = new THREE.Scene()
  * Objects
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const material = new THREE.MeshBasicMaterial({ color: 'gray' })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-/**
- * Sizes
- * 
- */
-const sizes = {
-    width: 800,
-    height: 600
-}
 
-/**
- * Camera
- */
+/*** Camera */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+// camera.position.x = 2
+// camera.position.y = 2
 camera.position.z = 3
+camera.lookAt(mesh.position)
+
 scene.add(camera)
+
+/*Controls*/
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
 /**
  * Renderer
  */
@@ -40,22 +65,24 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 }) //Gsap
-
-
 const clock = new THREE.Clock();
 //Animations
 const tick = () => {
 
     //Time
-    // const elapsedTime = clock.getElapsedTime()
-    // console.log(elapsedTime);
+    const elapsedTime = clock.getElapsedTime()
 
     // //Update Objects
-    // // mesh.rotation.y = elapsedTime  * Math.PI * 2  
-    // mesh.position.y = Math.sin(elapsedTime)
-    // mesh.position.x = Math.cos(elapsedTime)
+    //  mesh.rotation.y = elapsedTime
+
+    //Update Camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+
+    //update Controls
+    controls.update()
 
     //Renders
     renderer.render(scene, camera)
