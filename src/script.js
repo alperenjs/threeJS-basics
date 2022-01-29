af3e9edfaf9af17ebe148c39df7ea6cc08185f33
alperenkarate <alperensozen96@gmail.com>
2022-01-29 12:27:38 +0300

    object3d methods - Pi - scaling, positioning, axesHelper

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 import './style.css'
 import * as THREE from 'three'
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -8,12 +7,33 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Objects
+ * Objects --- ! const group = new THREE.Group() ile birden fazla objecti encapsulate edebiliriz.
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
+
+//Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.7
+// mesh.position.z = 0.7
+
+mesh.position.set(1, 1.5, 1)
+
+//Scale
+// mesh.scale.x = 1.5
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(1.5, 0.5, 0.5)
+
+//Rotations
+mesh.rotation.x = Math.PI * 0.25
+
+//Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
 
 /**
  * Sizes
@@ -28,7 +48,11 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+camera.position.y = 1
+camera.position.x = 1
 scene.add(camera)
+
+camera.lookAt(mesh.position)
 
 /**
  * Renderer
