cddfcfda154cad20b5ac58783cc2533b73ce1888
alperenkarate <alperensozen96@gmail.com>
2022-01-31 21:25:14 +0300

    added assets

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
 import * as lil from 'lil-gui'
+import { PointLightHelper } from 'three'
 // import image from '../static/textures/door/color.jpg'
 /**
 Textures 
@@ -27,11 +28,13 @@ loadingManager.onError = () => {
 const textureLoader = new THREE.TextureLoader(loadingManager)
 // const colorTexture = textureLoader.load('/textures/checkerboard-8x8.png')
 const colorTexture = textureLoader.load('/textures/minecraft.png')
+const doorColorTexture = textureLoader.load('/textures/door/alpha.jpg')
 const alphaTexture = textureLoader.load('/textures/door/alpha.jpg')
 const heightTexture = textureLoader.load('/textures/door/height.jpg')
 const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 const metalness = textureLoader.load('/textures/door/metalness.jpg')
 const normalTexture = textureLoader.load('/textures/door/normal.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
 const ambientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
 
 
@@ -49,7 +52,7 @@ colorTexture.magFilter = THREE.NearestFilter //SHARPEN PIXEL
 /* Sizes */
 const sizes = {
     width: window.innerWidth,
-    height: window.innerWidth
+    height: window.innerHeight
 }
 
 window.addEventListener('resize', () => {
@@ -79,13 +82,37 @@ window.addEventListener('mousemove', (event) => {
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
+
 // Scene
 const scene = new THREE.Scene()
+
+
+//Light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /** Objects */
 // const geometry = new THREE.BoxGeometry(1, 1, 1)
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ map: colorTexture })
+
+// const material = new THREE.MeshBasicMaterial({ map: colorTexture })
+// const material = new THREE.MeshNormalMaterial()
+
+// const material = new THREE.MeshMatcapMaterial()
+// material.matcap = matcapTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness = 0.56
+material.roughness = 0.65
+// material.map = doorColorTexture
+
+
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
@@ -117,7 +144,7 @@ const tick = () => {
     //Renders
     renderer.render(scene, camera)
 
-
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + .1 })
     window.requestAnimationFrame(tick)
 }
 
@@ -125,7 +152,6 @@ tick()
 
 //#region DEBUG //
 const gui = new lil.GUI()
-gui.hide()
 
 const parameters = {
     color: 0xff0000,
@@ -134,30 +160,20 @@ const parameters = {
     }
 }
 
-// gui.add(mesh.rotation, 'y', -3, 3, 0.01)
 gui
-    .add(mesh.rotation, 'y')
-    .min(-3)
-    .max(3)
+    .add(material, 'metalness')
+    .min(0)
+    .max(1)
     .step(0.01)
-    .name('rotation')
+    .name('Metallik')
 
-gui
-    .add(mesh, 'visible')
+    gui
+    .add(material, 'roughness')
+    .min(0)
+    .max(1)
+    .step(0.01)
+    .name('ROughness')
 
-gui
-    .add(material, 'wireframe')
-
-gui
-    .addColor(parameters, 'color')
-    .onChange(() => {
-        material.color.set(parameters.color)
-    })
-
-gui
-    .add(parameters, 'spin')
-    .name('döndür')
 
     //#endregion //
-	
-	
+
